768840ce6942c7f6d9bc238a0b3fb799a61ede00
Jonas Abreu <jonas@vidageek.net>
2012-07-03 16:38:34 -0300

    jekyll, script de deploy e mais umas coisas

---
+++ config.rb
@@ -0,0 +1,6 @@
+http_path = "/"
+css_dir = "stylesheets"
+images_dir = "images"
+javascripts_dir = "javascripts"
+output_style = :expanded
+line_comments = false
---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+jekyll || /var/lib/gems/1.8/bin/jekyll
+
+DOMAIN=www.dojosp.org
+
+cd _site;
+
+typeFor(){
+	TYPE=`echo $1 | sed -E "s/.+\.([^$]+)$/\1/"`
+	CONTENT_TYPE="text/html"
+	if [ $TYPE == "txt" ]; then CONTENT_TYPE="text/plain"; fi;
+	if [ $TYPE == "xml" ]; then CONTENT_TYPE="application/xml"; fi;
+	if [ $TYPE == "css" ]; then CONTENT_TYPE="text/css"; fi;
+	if [ $TYPE == "js" ]; then CONTENT_TYPE="text/javascript"; fi;
+	if [ $TYPE == "png" ]; then CONTENT_TYPE="image/png"; fi;
+}
+
+for X in `find . -type f | sed -E "s/\.\///"`; do 
+	echo "Deploying $X";
+	typeFor $X
+	~/bin/aws/aws put "x-amz-acl: public-read" "Content-Type: $CONTENT_TYPE" www.dojosp.org $X;
+done;
+
+curl "http://feedburner.google.com/fb/a/pingSubmit?bloglink=http%3A%2F%2Fwww.dojosp.org"
