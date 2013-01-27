b0f9db571c5784bdc9c9ea92b7be55dc1e682cdb
Jonas Abreu <jonas@vidageek.net>
2013-01-27 01:47:25 -0200

    melhoria no script de deploy

--- deploy.sh
+++ deploy.sh
@@ -2,26 +2,6 @@
 
 jekyll || /var/lib/gems/1.8/bin/jekyll
 
-DOMAIN=www.dojosp.org
-
-cd _site;
-
-typeFor(){
-	TYPE=`echo $1 | sed -E "s/.+\.([^$]+)$/\1/"`
-	CONTENT_TYPE="text/html"
-	if [ $TYPE == "txt" ]; then CONTENT_TYPE="text/plain"; fi;
-	if [ $TYPE == "xml" ]; then CONTENT_TYPE="application/xml"; fi;
-	if [ $TYPE == "css" ]; then CONTENT_TYPE="text/css"; fi;
-	if [ $TYPE == "js" ]; then CONTENT_TYPE="text/javascript"; fi;
-	if [ $TYPE == "png" ]; then CONTENT_TYPE="image/png"; fi;
-	if [ $TYPE == "gif" ]; then CONTENT_TYPE="image/gif"; fi;
-	if [ $TYPE == "zip" ]; then CONTENT_TYPE="application/zip"; fi;
-}
-
-for X in `find . -type f | sed -E "s/\.\///"`; do 
-	echo "Deploying $X";
-	typeFor $X
-	~/bin/aws/aws put "x-amz-acl: public-read" "Content-Type: $CONTENT_TYPE" www.dojosp.org $X;
-done;
+/home/ubuntu/bin/s3-jekyll-deploy/s3-jekyll-deploy www.dojosp.org
 
 curl "http://feedburner.google.com/fb/a/pingSubmit?bloglink=http%3A%2F%2Fwww.dojosp.org"
